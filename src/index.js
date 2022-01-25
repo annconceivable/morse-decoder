b4684f56291bd192d52977997120bbdf5557523d
annconceivable <84342516+annconceivable@users.noreply.github.com>
2022-01-25 11:03:54 +0300

    init: solve morse code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprChar = '';
+    let res = '';
+    const convertStr = (str) => {
+        let res = '';
+        for (let i = 0; i < str.length; i += 2) {
+            if (str[i] + str[i + 1] === '10') {
+                res = res + '.';
+            }
+            else {
+                res = res + '-';
+            }
+        }
+        return res;
+    }
+    for (let i = 0; i < expr.length; i += 10) {
+        if ( expr.slice(i, i + 10) === '**********' ) {
+            res = res + ' ';
+        }
+        else {
+            exprChar = String( Number(expr.slice(i, i + 10)) );
+            res = res + MORSE_TABLE[convertStr(exprChar)];
+        }
+    }
+    return res;
 }
 
 module.exports = {
